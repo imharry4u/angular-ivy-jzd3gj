fabe962deb0675294b2e308ed9ef9e4b238fb121
imharry4u <patelharish@gmail.com>
2020-07-10 17:01:14 -0400

    1 file modified

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,37 +1,15 @@
-// import { Component, OnChanges } from "@angular/core";
-
-// @Component({
-//   selector: "pm-star",
-//   templateUrl: "./star.component.html",
-//   styleUrls: ["./star.component.css"]
-// })
-// export class StarComponent implements OnChanges {
-//   rating: number = 4;
-//   starWidth: number;
-
-//   ngOnChanges(): void {
-//     this.starWidth = this.rating * 75 / 75;
-//   }
-// }
-import { Component, OnChanges, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnChanges } from "@angular/core";
 
 @Component({
-  selector: 'pm-star',
-  templateUrl: './star.component.html',
-  styleUrls: ['./star.component.css']
+  selector: "pm-star",
+  templateUrl: "./star.component.html",
+  styleUrls: ["./star.component.css"]
 })
 export class StarComponent implements OnChanges {
-  @Input() rating = 0;
-  starWidth = 0;
-  @Output() ratingClicked: EventEmitter<string> =
-    new EventEmitter<string>();
+  rating: number = 4;
+  starWidth: number;
 
   ngOnChanges(): void {
-    this.starWidth = this.rating * 75 / 5;
-  }
-
-  onClick(): void {
-    this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
+    this.starWidth = this.rating * 75 / 75;
   }
 }
-
