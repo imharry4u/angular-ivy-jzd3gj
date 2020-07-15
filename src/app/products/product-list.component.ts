c0b5091b3a8e2c9709b2e3852734e1e7a8827364
imharry4u <patelharish@gmail.com>
2020-07-15 08:03:24 -0400

    2 files modified

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
   selector: "pm-products",
@@ -22,70 +23,14 @@ export class ProductListComponent implements OnInit {
       : this.products;
   }
   filteredProducts: IProduct[];
-  products: IProduct[] = [
-    {
-      productId: 1,
-      productName: "Leaf Rake",
-      productCode: "GDN-0011",
-      releaseDate: "March 19, 2019",
-      description: "Leaf rake with 48-inch wooden handle.",
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl:
-        "https://th.bing.com/th/id/OIP.fin2aCrUa7WjEGye0exIBgHaK-?pid=Api&rs=1"
-    },
-    {
-      productId: 2,
-      productName: "Garden Cart",
-      productCode: "GDN-0023",
-      releaseDate: "March 18, 2019",
-      description: "15 gallon capacity rolling garden cart",
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl:
-        "https://www.clipartbay.com/cliparts/number-2-clip-art-4b6i2ib.png"
-    },
-    {
-      productId: 5,
-      productName: "Hammer",
-      productCode: "TBX-0048",
-      releaseDate: "May 21, 2019",
-      description: "Curved claw steel hammer",
-      price: 8.9,
-      starRating: 4.8,
-      imageUrl:
-        "https://clipartix.com/wp-content/uploads/2016/09/Numbers-cute-number-three-clipart-image.png"
-    },
-    {
-      productId: 8,
-      productName: "Saw",
-      productCode: "TBX-0022",
-      releaseDate: "May 15, 2019",
-      description: "15-inch steel blade hand saw",
-      price: 11.55,
-      starRating: 3.7,
-      imageUrl: "https://clipground.com/images/numbers-clipart-png-6.png"
-    },
-    {
-      productId: 10,
-      productName: "Video Game Controller",
-      productCode: "GMG-0042",
-      releaseDate: "October 15, 2018",
-      description: "Standard two-button video game controller",
-      price: 35.95,
-      starRating: 4.6,
-      imageUrl:
-        "https://th.bing.com/th/id/OIP.MeeVF3lwv5dk17wzDhLhAQHaJ5?pid=Api&rs=1"
-    }
-  ];
+  products: IProduct[] = [];
 
-  constructor(){
-    this.filteredProducts=this.products;
-    this.listFilter='cart';
+  constructor(private productService: ProductService) {
+    //this.listFilter = "cart";
   }
 
-  onRatingClicked(message:string):void{
-    this.pageTitle='Product List : ' + message;
+  onRatingClicked(message: string): void {
+    this.pageTitle = "Product List : " + message;
   }
 
   performFilter(filterBy: string): IProduct[] {
@@ -99,6 +44,8 @@ export class ProductListComponent implements OnInit {
     this.showImage = !this.showImage;
   }
   ngOnInit(): void {
-    console.log("In OnInit");
+    //console.log("In OnInit");
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
   }
 }
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { IProduct } from "./product";
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class ProductService {
   getProducts(): IProduct[] {
