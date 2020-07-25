54215afaef763c8f5820dacb4e11f012fbb9c21b
imharry4u <patelharish@gmail.com>
2020-07-24 22:48:59 -0400

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,13 +13,3 @@ export class AppComponent {
   pageTitle: string = "ACME Product Management";
 }
 
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'pm-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title = 'Angular: Getting Started';
-// }
