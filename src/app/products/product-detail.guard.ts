1efc7a44566f01a70b7f08c5b518f36b50886b73
imharry4u <patelharish@gmail.com>
2020-08-04 17:32:31 -0400

    2 files modified

--- src/app/products/product-detail.guard.ts
+++ src/app/products/product-detail.guard.ts
@@ -17,7 +17,7 @@ export class ProductDetailGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
-    let id = +next.url[1].path;
+    let id = +next.url[1].path;    
     if (isNaN(id) || id < 1) {
       alert("Invalid product Id");
       this.router.navigate(["/products"]);
