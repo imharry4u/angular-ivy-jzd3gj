f4a108d8fec33d0dfb9ce32307b8a86ed2888b15
imharry4u <patelharish@gmail.com>
2020-08-04 16:28:38 -0400

    3 files modified

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,13 +1,28 @@
 import { OnInit, Component } from "@angular/core";
 import { IProduct } from "./product";
+import { ActivatedRoute } from "@angular/router";
 
-@Component({  
+@Component({
   templateUrl: "./product-detail.component.html",
   styleUrls: ["./product-detail.component.css"]
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = "Product Detail";
   product: IProduct;
-  constructor() {}
-  ngOnInit() {}
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get("id");
+    this.pageTitle += ` : ${id}`;
+    this.product = {
+      productId: id,
+      productName: "Garden Cart",
+      productCode: "GDN-0023",
+      releaseDate: "March 18, 2019",
+      description: "15 gallon capacity rolling garden cart",
+      price: 32.99,
+      starRating: 4.2,
+      imageUrl:
+        "https://www.clipartbay.com/cliparts/number-2-clip-art-4b6i2ib.png"
+    };
+  }
 }
