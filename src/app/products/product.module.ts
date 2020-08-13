fc942f8e4215d3a5834714b071a231fe5b15ff04
imharry4u <patelharish@gmail.com>
2020-08-13 16:45:00 -0400

    2 files modified

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { ProductListComponent } from "./product-list.component";
+import { ConvertToSpacesPipe } from "../shared/convert-to-spaces.pipe";
+import { StarComponent } from "../shared/star.component";
+import { ProductDetailComponent } from "./product-detail.component";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { ProductDetailGuard } from "./product-detail.guard";
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: []
+  imports: [CommonModule, FormsModule, RouterModule.forChild([
+    { path: "products", component: ProductListComponent },
+      {
+        path: "products/:id",
+        canActivate: [ProductDetailGuard],
+        component: ProductDetailComponent
+      },
+  ])],
+  declarations: [
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent,
+    ProductDetailComponent
+  ]
 })
 export class ProductModule {}
