85e9f99ab7a93f2baa06380a4442a009929ba045
imharry4u <patelharish@gmail.com>
2021-01-15 15:50:36 -0500

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,29 @@ import { Component } from "@angular/core";
 @Component({
   selector: "pm-root",
   template: `
-    <nav class="navbar navbar-expand navbar-light bg-light">
-      <a class="navbar-barnd">{{ pageTitle }}</a>
+    <nav
+      class="navbar navbar-expand navbar-primary bg-primary"
+      style="justify-content: center;background-color: lightblue;"
+    >
+      <h3 class="text-white">{{ pageTitle }}</h3>
       <ul class="nav nav-pills">
         <li>
-          <a class="nav-link" [routerLink]="['/welcome']">Home</a>
+          <a class="text-warning" [routerLink]="['/welcome']">Home</a>
         </li>
         <li>
-          <a class="nav-link" [routerLink]="['/products']">Product List</a>
+          <a class="text-warning" [routerLink]="['/products']">Product List</a>
         </li>
       </ul>
     </nav>
     <div class="container">
       <router-outlet></router-outlet>
     </div>
+    <footer class="border-top footer text-white bg-primary">
+      <div style="text-align:center">
+        &copy; 2021 - ACME Product Management -
+        <a asp-area="" asp-controller="Home" asp-action="Privacy">Privacy</a>
+      </div>
+    </footer>
   `
 })
 export class AppComponent {
